c1fb4416894ce2c3c1f075700a3de7f82747b89a
inna <aleksandr9311737@mail.ru>
2026-03-10 15:38:48 +0300

    Добавила счетчик

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,36 @@
 import { useState } from 'react'
-
 import Header from './components/UI/Header/Header'
-
 
 function App() {
   const [count, setCount] = useState(0)
-return (
-<>
-  <Header />
-  <h1>Счетчик кликов</h1>
-    <button onClick={() => setCount(count + 1)}>
-      Клик: {count}
-    </button>
-</>
-)
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <h1>Счетчик кликов</h1>
+      <div>
+        <p>Текущее значение: {count}</p>
+        
+        <button onClick={() => setCount(count + 1)}>
+          +1
+        </button>
+
+        <button onClick={handleDecrement}>
+          -1
+        </button>
+
+        <button onClick={() => setCount(0)}>
+          Сбросить
+        </button>
+      </div>
+    </>
+  )
 }
+
 export default App
